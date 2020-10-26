dc423c2f680a940f4bca4b3c2216e80790c0ea59
abaker93 <anna.baker.design@gmail.com>
2020-10-26 12:10:06 -0400

    dropdown hover effects

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const 
+const megaMenu = document.getElementById('mega-menu')
+
+megaMenu.addEventListener('mouseenter', () => {
+    document.getElementById('mega-menu-submenu').classList.add('show')
+})
+
+megaMenu.addEventListener('mouseleave', () => {
+    document.getElementById('mega-menu-submenu').classList.remove('show')
+})
+
+const dropdown = document.getElementById('dropdown')
+
+dropdown.addEventListener('mouseenter', () => {
+    document.getElementById('dropdown-submenu').classList.add('show')
+})
+
+dropdown.addEventListener('mouseleave', () => {
+    document.getElementById('dropdown-submenu').classList.remove('show')
+})
