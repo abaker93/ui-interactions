d554a27f0a6de90820c53d788ef2cdcf9758ba92
abaker93 <anna.baker.design@gmail.com>
2020-10-26 22:26:57 -0400

    slider functionality

--- main.js
+++ main.js
@@ -31,3 +31,35 @@ mobileToggle.addEventListener('click', () => {
     }
 })
 
+let index = 0
+const slides = document.querySelectorAll('.slide')
+const dots = document.querySelectorAll('.dot')
+
+const changeSlide = () => {
+    if (index < 0) {
+        index = slides.length-1
+    }
+
+    if (index > slides.length-1) {
+        index = 0
+    }
+
+    console.log(slides[index])
+    console.log(dots[index])
+
+    for (let i=0; i<slides.length; i++) {
+        slides[i].style.visibility = 'hidden'
+        slides[i].style.opacity = '0'
+        dots[i].classList.remove('active')
+    }
+
+    slides[index].style.visibility = 'visible'
+    slides[index].style.opacity= '1'
+    dots[index].classList.add('active')
+
+    index++;
+
+    setTimeout(changeSlide, 5000)
+}
+
+changeSlide()
